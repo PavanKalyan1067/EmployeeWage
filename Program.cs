ddf9d2c19ee75143fb1ef9162b5637f0605fe012
Pavan Kalyan <nikhilgoud@nikhils-MacBook-Air.local>
2022-04-02 03:12:12 +0530

    Employee Attendence using Random

--- Program.cs
+++ Program.cs
@@ -7,6 +7,31 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Employee Wage Compututions!");
+
+            //local variable
+            int employeePresent = 1;
+            int empHours = 0;
+            int empRatePerHour = 20;
+            int empWage = 0;
+            //creating object
+            Random random = new Random();
+            //calling Next() for generating random values
+            int Attendance = random.Next(0, 2);
+            Console.WriteLine("Random number is :" + Attendance);
+
+            if (Attendance == employeePresent)
+            {
+                Console.WriteLine("Employee is Present \n");
+                empHours = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent \n");
+                empHours = 0;
+
+            }
+            empWage = empRatePerHour * empHours;
+            Console.Read();
         }
     }
 }
