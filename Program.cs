d5378c1f7f992da0b9d846f7106299b652735539
Pavan Kalyan <nikhilgoud@nikhils-MacBook-Air.local>
2022-04-02 08:59:06 +0530

    Calculating Wages Using Switch Case

--- Program.cs
+++ Program.cs
@@ -4,34 +4,35 @@ namespace EmployeeWage
 {
     class Program
     {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUE = 20;
+
         static void Main(string[] args)
         {
-            //Constants
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
-            int EMP_RATE_PER_HOUE = 20;
             //Variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
+
             //Calculations
             int empCheck = random.Next(3);
-            if (empCheck == IS_FULL_TIME)
+            switch (empCheck)
             {
-                empHrs = 8;
-            }
-            else if (empCheck == IS_PART_TIME)
-            {
-                empHrs = 4;
-            }
-            else
-            {
-                empHrs = 0;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+
             }
             empWage = empHrs * EMP_RATE_PER_HOUE;
 
             Console.WriteLine("Emp Wage : " + empWage);
         }
     }
-
 }
