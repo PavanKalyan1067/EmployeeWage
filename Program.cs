43f7a1ee2183680b4a13fafa612b90aa8e13b2d3
Pavan Kalyan <nikhilgoud@nikhils-MacBook-Air.local>
2022-04-02 09:01:36 +0530

    Calculating Wages for a month

--- Program.cs
+++ Program.cs
@@ -7,32 +7,39 @@ namespace EmployeeWage
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUE = 20;
+        public const int WR_DAYS_IN_A_MONTH = 20;
+        private static int totalEmpWage;
 
         static void Main(string[] args)
         {
             //Variables
             int empHrs = 0;
             int empWage = 0;
-            Random random = new Random();
+            totalEmpWage = 0;
 
             //Calculations
-            int empCheck = random.Next(3);
-            switch (empCheck)
+            for (int day = 0; day < WR_DAYS_IN_A_MONTH; day++)
             {
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
 
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUE;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage : " + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUE;
-
-            Console.WriteLine("Emp Wage : " + empWage);
+            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
         }
     }
 }
