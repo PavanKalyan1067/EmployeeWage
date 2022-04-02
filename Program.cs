d0ccf3b5ccc9fc5edca15e05e4461f55b8c195db
Pavan Kalyan <nikhilgoud@nikhils-MacBook-Air.local>
2022-04-02 08:56:16 +0530

    Adding Parttime to Employee Wage

--- Program.cs
+++ Program.cs
@@ -8,24 +8,29 @@ namespace EmployeeWage
         {
             //Constants
             int IS_FULL_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
+            int IS_PART_TIME = 2;
+            int EMP_RATE_PER_HOUE = 20;
             //Variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            //Computation
-            int empCheck = random.Next(2);
+            //Calculations
+            int empCheck = random.Next(3);
             if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
             }
             else
             {
                 empHrs = 0;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Emp Wage : " + empWage);
+            empWage = empHrs * EMP_RATE_PER_HOUE;
 
+            Console.WriteLine("Emp Wage : " + empWage);
         }
     }
 
